5939ba1af3751fe6c0c9556c99a909040d47a5be
alejandr029 <alexvelazquez2812@gmail.com>
2023-06-30 15:48:28 -0700

    forst commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title>BeautySys</title>
+    </head>
+    
+    <main>
+        <section>
+            example
+        </section>
+
+        <section>
+            <?php
+                return view('welcome');
+            ?>
+        </section>
+
+    </main>
+
+    <footer></footer>
+</html>
