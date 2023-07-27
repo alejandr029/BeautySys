5c590d2b2c81110c78933cd5e03776d0b65e8ece
eduxal <eduardo.rual58@gmail.com>
2023-07-27 00:19:22 -0700

    adds styles to the role assign form

--- resources/views/assign-roles/index.blade.php
+++ resources/views/assign-roles/index.blade.php
@@ -1,30 +1,35 @@
-<!-- resources/views/asignar_roles/index.blade.php -->
 @extends('layout.tamplated')
 
 @section('content')
-    <h1>Asignar Roles</h1>
+<div class="container-fluid py-4">
+    <h1 class="mb-4">Asignar Roles</h1>
 
-    <!-- Formulario para asignar roles -->
-    <form action="{{ route('assign-roles.assign') }}" method="POST">
-        @csrf
-        <div>
-            <label for="usuario_id">Seleccionar Usuario:</label>
-            <select name="usuario_id" id="usuario_id">
-                @foreach($usuarios as $usuario)
-                    <option value="{{ $usuario->id }}">{{ $usuario->name }}</option>
-                @endforeach
-            </select>
+    <div class="row">
+        <div class="col-lg-6">
+            <!-- Formulario para asignar roles -->
+            <form action="{{ route('assign-roles.assign') }}" method="POST" class="role-form">
+                @csrf
+                <div class="mb-3">
+                    <label for="usuario_id" class="form-label">Seleccionar Usuario:</label>
+                    <select class="form-select" name="usuario_id" id="usuario_id">
+                        @foreach($usuarios as $usuario)
+                        <option value="{{ $usuario->id }}">{{ $usuario->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label for="rol_id" class="form-label">Seleccionar Rol:</label>
+                    <select class="form-select" name="rol_id" id="rol_id">
+                        @foreach($roles as $rol)
+                        <option value="{{ $rol->id }}">{{ $rol->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Asignar Rol</button>
+            </form>
         </div>
-
-        <div>
-            <label for="rol_id">Seleccionar Rol:</label>
-            <select name="rol_id" id="rol_id">
-                @foreach($roles as $rol)
-                    <option value="{{ $rol->id }}">{{ $rol->name }}</option>
-                @endforeach
-            </select>
-        </div>
-
-        <button type="submit">Asignar Rol</button>
-    </form>
+    </div>
+</div>
 @endsection
