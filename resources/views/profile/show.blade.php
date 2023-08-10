1b4545ff530977c0f0568ccf01a235cf84413671
Cristian22-11 <cristian.toledop02@gmail.com>
2023-08-10 13:02:44 -0700

    estilos perfil

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,11 +1,49 @@
-<x-app-layout>
+<style>
+    .backbutton{
+      width: 35px;
+      height: 35px;
+      border-radius: 50%;
+      background-color: #fff;
+      border: none;
+      font-weight: 600;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.164);
+      cursor: pointer;
+      transition-duration: .3s;
+      overflow: hidden;
+      position: relative;
+    }
+
+    header{
+        background: #EC407A!important;
+    }
+
+  </style>
+
+
+<x-app-layout >
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+        <div style="display: flex; align-items: center; gap:20px;">
+            <button id="backButton" class="backbutton" onclick="goBack()" style="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+              </button>
+              <script>
+                function goBack() {
+                  window.history.back();
+                }
+              </script>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Profile') }}
+                </h2>
+        </div>
     </x-slot>
 
-    <div>
+
+    <div style="background-image: url('../assets/img/logos/fondo.avif'); background-size: cover">
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
@@ -29,12 +67,12 @@
                 <x-section-border />
             @endif
 
-            <div class="mt-10 sm:mt-0">
+            {{-- <div class="mt-10 sm:mt-0">
                 @livewire('profile.logout-other-browser-sessions-form')
-            </div>
+            </div> --}}
 
             @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-                <x-section-border />
+                {{-- <x-section-border /> --}}
 
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.delete-user-form')
