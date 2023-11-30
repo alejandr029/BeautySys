0725bb7c7505080d9985a725a0a936f9996a8538
Cristian22-11 <cristian.toledop02@gmail.com>
2023-11-30 00:39:30 -0800

    FIX DE .ENV DE RUIZ!!!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
+    {{-- <meta http-equiv="Content--Policy" content="upgrade-insecure-requests"> --}}
 
     <link rel="shortcut icon" sizes="76x76" href="../assets/img/logos/logoproyecto8b.png">
     <link rel="icon" type="image/png" href="../assets/img/logos/logoproyecto8b">
